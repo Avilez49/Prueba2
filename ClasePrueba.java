74ee0cca2843d6afb7ad93dce49c7a3b5672e058
Fernando Avilez Tzec <diegooavilez4912@gmail.com>
2024-02-28 20:19:40 -0600

    Cambios dentro de la rama

--- ClasePrueba.java
+++ ClasePrueba.java
@@ -1,12 +1,22 @@
 package Paquete1;
 
+import java.util.Random;
+
 public class ClasePrueba {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		System.out.println("Ola");
 		
+		int Num;
+		Random numeroAleatorio = new Random();
+		System.out.println("Ola");
+		System.out.println( "Insertando los siguientes valores: " ); 
+		// inserta 10 enteros aleatorios de 0 a 99 en arbol
+	  for ( int i = 1; i <= 10; i++ )
+    {
+       Num = numeroAleatorio.nextInt( 100 );
+       System.out.print( Num + " " );
+    }
 	}
 
 }
